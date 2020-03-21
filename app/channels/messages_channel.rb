6ab43e93645ab4eefeb4227a86f78cc2f07f428a
DAICHI KUN <daichi@DAICHInoiMac.local>
2020-03-22 00:54:48 +0900

    Fixed redis connection

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -2,4 +2,8 @@ class MessagesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "chatroom_#{params['room']}"
   end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
 end
