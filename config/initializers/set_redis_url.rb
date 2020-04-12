13161358e8d40071c022ad09cceb004071397e5f
DAICHI KUN <daichi@DAICHInoiMac.local>
2020-04-12 10:33:05 +0900

    Change heroku path

--- config/initializers/set_redis_url.rb
+++ config/initializers/set_redis_url.rb
@@ -1,1 +1,2 @@
-ENV['REDIS_URL'] = Rails.application.credentials.redis_url  if  Rails.application.credentials.redis_url
+$redis = Redis.new(url: ENV["REDIS_URL"])
+# ENV['REDIS_URL'] = Rails.application.credentials.redis_url  if  Rails.application.credentials.redis_url
